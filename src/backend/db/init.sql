e52aa3b328375b2ba14da2a2008c1d351bdaddc7
MentalSmoge <mentalsmoge@gmail.com>
2025-04-05 16:06:24 +0500

    fixed init sql

--- src/backend/db/init.sql
+++ src/backend/db/init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
-  email VARCHAR(100)
+  email VARCHAR(100),
+  password VARCHAR(100)
 );
 
 INSERT INTO users (name, email) VALUES ('Docker User', 'docker@example.com');
