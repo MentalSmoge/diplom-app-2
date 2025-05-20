d64f7f1c9ee0ec416c55fff3ad76119b06712aa8
MentalSmoge <mentalsmoge@gmail.com>
2025-05-20 06:55:26 +0500

    huh22

--- src/.backend/init.sql
+++ src/.backend/init.sql
@@ -1,19 +1,3 @@
--- Database: userdb
-
--- DROP DATABASE IF EXISTS userdb;
-
-CREATE DATABASE userdb
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
--- Table: public.boards
-
 -- DROP TABLE IF EXISTS public.boards;
 
 CREATE TABLE IF NOT EXISTS public.boards
